40403902c86c9d2898c1663a7876b99afced23f1
Deshini Shivadatta <deshinishivadatta@gmail.com>
2026-04-06 03:07:09 +0530

    Debug DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL); // add this line
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
@@ -8,6 +10,6 @@ const pool = new Pool({
 
 pool.connect()
   .then(() => console.log('PostgreSQL connected successfully'))
-  .catch((err) => console.error('DB connection error:', err.message));
+  .catch((err) => console.error('DB connection error:', JSON.stringify(err, null, 2)));
 
 module.exports = pool;
